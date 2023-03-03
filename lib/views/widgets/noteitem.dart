e586bfc3cefd710974948951a9257052bf868bc1
Ali1910 <alihany5559@gmail.com>
2023-03-03 13:51:40 +0200

    noteitem created

---
+++ lib/views/widgets/noteitem.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Noteitem extends StatelessWidget {
+  const Noteitem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: const Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              'may21 , 2022',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.4), fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
