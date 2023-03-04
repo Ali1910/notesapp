93bc4fc0977d385daf8bfe2f2362106468759ee2
Ali1910 <alihany5559@gmail.com>
2023-03-04 12:00:46 +0200

    add custom appbar to editnotesview

---
+++ lib/views/widgets/Notes_edit_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:notesapp/views/widgets/editviewbody.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: EditViewBody(),
+    );
+  }
+}
