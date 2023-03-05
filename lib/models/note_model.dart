5b0a23356500da56f15e2bd2ac442dad77f3eb15
Ali1910 <alihany5559@gmail.com>
2023-03-05 14:32:38 +0200

    create typeadaptor

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 1)
 class Notemodel {
+  @HiveField(0)
   final String Title;
+  @HiveField(1)
   final String Subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int Color;
   Notemodel(
       {required this.Color,
